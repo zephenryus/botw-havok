29344bc2735e2cb6383544c8eaf2d5455ea05d27
Kevin Jensen <kevin@turtlerockweb.com>
2019-01-21 11:04:44 -0700

    added header tests and reverted to older file structure

--- havok/Havok.py
+++ havok/Havok.py
@@ -1,21 +1,20 @@
-from typing import BinaryIO
-
-from havok.DataSegment import DataSegment
-from havok.ClassNames import ClassNames
-from havok.Header import Header
-from havok.SegmentHeaderOffsetTables import SegmentHeaderOffsetTables
+from .DataSegment import DataSegment
+from .ClassNames import ClassNames
+from .Header import Header
+from .SegmentHeaderOffsetTables import SegmentHeaderOffsetTables
 
 
 class Havok:
     header: Header
     segment_header_offset_tables: SegmentHeaderOffsetTables
 
-    def __init__(self, infile: BinaryIO) -> None:
-        self.header = Header(infile)
-        self.segment_header_offset_tables = SegmentHeaderOffsetTables(infile)
-        self.classnames = ClassNames(infile, self.segment_header_offset_tables.classnames)
-        self.data_segment = DataSegment(infile, self.segment_header_offset_tables.data)
-        self.data = Data(infile, self.data_segment, self.classnames)
+    def __init__(self, path: str) -> None:
+        with open(path, 'rb') as infile:
+            self.header = Header(infile)
+            self.segment_header_offset_tables = SegmentHeaderOffsetTables(infile)
+            self.classnames = ClassNames(infile, self.segment_header_offset_tables.classnames)
+            self.data_segment = DataSegment(infile, self.segment_header_offset_tables.data)
+            self.data = Data(infile, self.data_segment, self.classnames)
 
     def __repr__(self):
         return "{} <header: {}, segment_header_offset_tables: {}, classnames: {}>".format(
